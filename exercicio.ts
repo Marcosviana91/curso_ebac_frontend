f9747a8319fcba3615555f5b6a4a012eb06c8998
Marcos Viana <97001316+Marcosviana91@users.noreply.github.com>
2023-08-13 23:54:07 -0300

    Add files via upload

---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicacao (num1: number, num2: number) : number {
+    return num1 * num2;
+}
+
+function saudacao (nome: string) : string {
+    return "Olá " + nome
+}
+
+console.log(multiplicacao(10, 2));
+console.log(saudacao('Marcos'));
+
